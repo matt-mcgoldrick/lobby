092d00c615c8634c182e1c700ac9f885a9afcf11
matt <mtmcgoldrick1@gmail.com>
2020-01-11 15:28:16 -0500

    responsive inputs

--- homepage.js
+++ homepage.js
@@ -1,9 +1,31 @@
-// get a list of all cards
 var cardList = document.querySelectorAll(".card");
-var twitchIcons = document.querySelectorAll("streamer");
-var streamer1 = document.querySelector("#streamerName1");
-var streamer2 = document.querySelector("#streamerName2");
-var streamer3 = document.querySelector("#streamerName3");
+var editButton1 = document.querySelector("#editButton1");
+var editButton2 = document.querySelector("#editButton2");
+var editButton3 = document.querySelector("#editButton3");
+var streamerNameInput1 = document.querySelector("#streamerNameInput1");
+var streamerNameInput2 = document.querySelector("#streamerNameInput2");
+var streamerNameInput3 = document.querySelector("#streamerNameInput3");
+var streamerName1 = document.querySelector("#streamerName1");
+var streamerName2 = document.querySelector("#streamerName2");
+var streamerName3 = document.querySelector("#streamerName3");
+var streamerIcon1 = document.querySelector("#streamerIcon1");
+var streamerIcon2 = document.querySelector("#streamerIcon2");
+var streamerIcon3 = document.querySelector("#streamerIcon3");
+var streamerList = [];
+var editButtonList = $(".edit");
+
+class Streamer {
+    constructor(url, isLive, name, _icon, _span, _edit, _input)
+    {
+        this.url = url;
+        this.isLive = isLive;
+        this.name = name;
+        this._icon = _icon;
+        this._span = _span;
+        this._edit = _edit;
+        this._input = _input;
+    }
+}
 
 init();
 
@@ -30,6 +52,73 @@ window.addEventListener("resize", function () {
     }
 });
 
+// Adjust shading of cards dynamically as window is resized by user
+window.addEventListener("resize", () => {
+    dynamicShadows();
+    dynamicInputs();
+});
+
+function dynamicShadows() {
+    if (window.innerWidth < 768) {
+        //remove shading on 2nd to last card when window is xs-sm
+        cardList[cardList.length - 2].classList.remove("shadow");
+        cardList[cardList.length - 2].classList.add("shadow-sm");
+    }
+    else if (window.innerWidth < 992) {
+        //remove shading on 3rd to last card when window is md 
+        cardList[cardList.length - 3].classList.remove("shadow");
+        cardList[cardList.length - 3].classList.add("shadow-sm");
+
+        //add shading back to 2nd to last card
+        cardList[cardList.length - 2].classList.add("shadow");
+        cardList[cardList.length - 2].classList.remove("shadow-sm");
+    }
+    else {
+        //add shading back to 3rd to last card
+        cardList[cardList.length - 3].classList.add("shadow");
+        cardList[cardList.length - 3].classList.remove("shadow-sm");
+    }
+}
+
+function dynamicInputs() {
+    if(window.innerWidth < 768) {
+        $("input").css("font-size", "25px");
+    }
+    else if (window.innerWidth < 992) {
+        $("input").css("font-size", "18px");
+    }
+    else {
+        $("input").css("font-size", "25px");   
+    }
+}
+
+streamerNameInput1.addEventListener("input", function(input) {
+    userInputUrl = input.value;
+});
+
+// Listen for user to click any of the edit buttons and accept user input
+$(".edit").on( "click", function() {
+    var editbuttons = $(this);
+    streamerList.forEach(function(strmr) {
+        if(strmr._edit.id == editbuttons.attr('id')) {
+            strmr._span.classList.add("d-none");
+            strmr._input.classList.remove("d-none");
+            strmr.editStreamer(_input.value);
+        }
+    });
+});
+
+// function that is called when adding a streamer
+function addStreamer(url, icon, name, _icon, _span, _edit, _input) {
+    var streamer = new Streamer(url, icon, name, _icon, _span, _edit, _input);
+    streamerList.push(streamer);
+}
+
+// function that is called when editing a streamer
+function editStreamer(url) {
+    this.url = url;
+}
+
 // Add shadow to cards as component of hover animation
 $(".card").hover(
     function() {
@@ -40,34 +129,12 @@ $(".card").hover(
 );
 
 function getStreamerInfo() {  
-    $.get("https://www.twitch.tv/siritron", function() {
-
-    })
+    $.get("https://www.twitch.tv/siritron", function(){
+    });
 }
- 
 
 function init() {
-    addStreamer("https://www.twitch.tv/siritron", twitchIcons[0], "siritron");
-    addStreamer("https://www.twitch.tv/xcaliz0rz", twitchIcons[1], "xcaliz0rz");
-    addStreamer("https://www.twitch.tv/pestily", twitchIcons[2], "pestily");
-}
-
-// function that is called when adding a streamer
-function addStreamer(url, icon, name) {
-    var streamer = new Streamer(url);
-    streamerList.add(streamer);
-    
-}
-
-function editStreamer(url, icon, name) {
-
-}
-
-class Streamer {
-    constructor(url, icon, name)
-    {
-        this.url = url;
-        this.icon = icon;
-        this.name = name;
-    }
+    addStreamer("https://www.twitch.tv/siritron", false, "siritron", streamerIcon1, streamerName1, editButton1, streamerNameInput1);
+    addStreamer("https://www.twitch.tv/xcaliz0rz", true, "xcaliz0rz", streamerIcon2, streamerName2, editButton2, streamerNameInput2);
+    addStreamer("https://www.twitch.tv/pestily", false, "pestily", streamerIcon3, streamerName3, editButton3, streamerNameInput3);
 }
