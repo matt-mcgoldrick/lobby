b165dc6cbd044a6a3c555668ff01e5b7665062c0
Matt McGoldrick <mtmcgoldrick1@gmail.com>
2020-05-22 21:56:22 -0400

    RESTful routing refactor pt.1

--- lobby.js
+++ lobby.js
@@ -1,29 +1,32 @@
 const express = require("express"),
       request = require("request"),
+      methodOverride = require("method-override"),
+      expressSanitizer = require("express-sanitizer"),
       bodyParser = require('body-parser'),
-      mongoose = require("mongoose");
+      mongoose = require("mongoose"),
+      app = express();
 
 mongoose.connect("mongodb://localhost:27017/lobby_io", { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('useFindAndModify', false);
 
-const app = express();
-
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/public', express.static("public"));
-app.set("view engine", "ejs");
+app.use(expressSanitizer());
+app.use(methodOverride("_method"));
 
+// MONGOOSE/MODEL CONFIG
 const streamerSchema = new mongoose.Schema({
     login: String,
     url: String, 
-    isLive: Boolean
+    isLive: String
 });
-
 const Streamer = mongoose.model("Streamer", streamerSchema);
 
-/* Streamer.create({
+Streamer.create({
     login: "siritron",
     url: "https://www.twitch.tv/siritron",
-    isLive: "true"
+    isLive: "color:none"
 }, function(err, str){
     if(err){
         console.log(err);
@@ -34,8 +37,8 @@ const Streamer = mongoose.model("Streamer", streamerSchema);
 
 Streamer.create({
     login: "xcaliz0rz",
-    url: "https://www.twitch.tv/siritron",
-    isLive: "true"
+    url: "https://www.twitch.tv/xcaliz0rz",
+    isLive: "color:none"
 }, function(err, str){
     if(err){
         console.log(err);
@@ -46,21 +49,126 @@ Streamer.create({
 
 Streamer.create({
     login: "cirno_tv",
-    url: "https://www.twitch.tv/siritron",
-    isLive: "true"
+    url: "https://www.twitch.tv/cirno_tv",
+    isLive: "color:none"
 }, function(err, str){
     if(err){
         console.log(err);
     } else {
         console.log(str);
     }
-}); */
+});
+
+app.get("/", function(req, res){
+    res.redirect("/streamers");
+})
+
+// INDEX
+app.get("/about", function(req, res) {
+    res.render("about");
+});
+
+app.get("/blog", function(req, res) {
+    res.render("blog");
+});
+
+app.get("/streamers", function(req, res) {
+    Streamer.find({}, function(err, streamers) {
+        if(err) {
+            console.log(err);
+        } else {
+            checkIfLive(function(err, str){
+                if(err){
+                    console.log(err);
+                } else {
+                    console.log("HERE")
+                    console.log("str: " + str);
+                    console.log(streamers);   
+                }
+            });
+            res.render("homepage", {streamers: streamers});
+        }
+    });
+});
+
+// NEW
+app.get("/blog/new", function(req, res){
+    res.render("newBlog");
+});
+
+app.get("/streamers/new", function(req, res){
+    res.render("new");
+});
+
+// CREATE
+app.post("/blog", function(req, res){
+
+});
+
+app.post("/streamers", function(req, res) {
+
+});
+
+// SHOW
+app.get("/blog/:id", function(req, res) {
+    res.send("Blog post " + req.params.id);
+});
+
+app.get("/streamers/:id", function(req, res){
+    res.send("Streamer #" + req.params.id);
+});
+
+// EDIT
+app.get("/blog/:id/edit", function(req, res) {
+
+});
+
+app.get("/streamers/:id/edit", function(req, res) {
+
+});
+
+// UPDATE
+app.put("/blog/:id", function(req, res) {
+
+});
+
+app.put("/streamers/:id", function(req, res){
+    const streamer1 = req.body.streamer1;
+    const options = { new: true};
+    Streamer.update({}, { login: streamer1, url: "https://www.twitch.tv/" + streamer1 }, options, function(err, doc){
+        if(err){
+            console.log(err);
+        } else {
+            console.log(doc);
+            res.redirect("/streamers");
+        }
+    });
+
+});
+
+// DESTROY
+app.delete("/blog/:id", function(req, res){
+    
+})
+
+app.delete("/streamers/:id", function(req, res){
+
+});
+
+
+class streamerIcon {
+    constructor(iconElement, isLive) {
+        this.iconElement = iconElement;
+        this.isLive = isLive;
+    }
+}
 
 function checkIfLive(){
     Streamer.find({}, function(err, str) {
         if(err) {
             console.log(err);
         } else {
+            console.log("str:: " + str);
             str.forEach(function(streamer){
                 options.url = 'https://api.twitch.tv/helix/streams?user_login=' + streamer.login;
                 request(options, callback);
@@ -76,119 +184,26 @@ const options = {
     }    
 };
 
-// isLive logic not working + icon colors not changing
-app.get("/", function(req, res) {
-    Streamer.find({}, function(err, str) {
-        if(err) {
-            console.log(err);
-        } else {
-            let isLive1 = {};
-            let isLive2 = {};
-            let isLive3 = {};
-            if(str[0].isLive == true){
-                isLive1 = { liveStatus :"color:red" }
-            } else {
-                isLive1 = { liveStatus :"color:none" }
-            }
-            if(str[1].isLive == true){
-                isLive2 = { liveStatus :"color:red" }
-            } else {
-                isLive2 = { liveStatus :"color:none" }
-            }
-            if(str[2].isLive == true){
-                isLive3 = { liveStatus :"color:red" }
-            } else {
-                isLive3 = { liveStatus :"color:none" }
-            }
-            console.log(isLive1);
-            console.log(isLive2);
-            console.log(isLive3);
-            checkIfLive();
-            res.render("homepage", {streamer1: str[0].login, streamer2: str[1].login, streamer3: str[2].login, 
-                isLive1: isLive1.liveStatus, isLive2: isLive2.liveStatus, isLive3: isLive3.liveStatus});
-        }
-    });
-});
-
-app.get("/about", function(req, res) {
-    res.render("about");
-});
-
-app.get("/blog", function(req, res) {
-    res.render("blog");
-});
-
-app.get("/blog/:postNumber", function(req, res) {
-    res.send("Post " + req.params.postNumber);
-});
-
-app.post("/editstreamer1", function(req, res){
-    const str1 = req.body.newstreamer1;
-    const query = { num: 1 };
-    const options = { new: true};
-    Streamer.findOneAndUpdate(query, { login: str1, url: "https://www.twitch.tv/" + str1 }, options, function(err, doc){
-        if(err){
-            console.log(err);
-        } else {
-            console.log(doc);
-            res.redirect("/");
-        }
-    });
-});
-
-app.post("/editstreamer2", function(req, res){
-    const str2 = req.body.newstreamer2;
-    const query = { num: 2 };
-    const options = { new: true};
-    Streamer.findOneAndUpdate(query, { login: str2, url: "https://www.twitch.tv/" + str2 }, options, function(err, doc){
-        if(err){
-            console.log(err);
-        } else {
-            console.log(doc);
-            res.redirect("/");
-        }
-    });
-});
-
-app.post("/editstreamer3", function(req, res){
-    const str3 = req.body.newstreamer3;
-    const query = { num: 3 };
-    const options = { new: true};
-    Streamer.findOneAndUpdate(query, { login: str3, url: "https://www.twitch.tv/" + str3 }, options, function(err, doc){
-        if(err){
-            console.log(err);
-        } else {
-            console.log(doc);
-            res.redirect("/");
-        }
-    });
-});
-
-class streamerIcon {
-    constructor(iconElement, isLive) {
-        this.iconElement = iconElement;
-        this.isLive = isLive;
-    }
-}
-
 function callback(error, response, body) {
     if (!error && response.statusCode == 200) {
         const info = JSON.parse(body);
         if (info.data.length !== 0) {
             const query = {login: this.uri.query.substr(11)};
-            Streamer.findOneAndUpdate(query, {isLive: true}, function(err, str) {
+            Streamer.findOneAndUpdate(query, {isLive: "color:red" }, function(err, str) {
                 if (err) {
                     console.log(err);
                 } else {
+                    console.log(str);
                 }
             });
         }
         else {
             const query = {login: this.uri.query.substr(11)};
-            Streamer.findOneAndUpdate(query, {isLive: false}, function(err, str) {
+            Streamer.findOneAndUpdate(query, {isLive: "color:none" }, function(err, str) {
                 if (err) {
                     console.log(err);
                 } else { 
+                    console.log(str);
                 }
             });
         }
@@ -199,4 +214,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, function(){
     console.log("Enter through the lobby.");
 });
-
