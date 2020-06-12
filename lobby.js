82351fbe151f12121c2b2a17325a2f3abc987132
Matt McGoldrick <mtmcgoldrick1@gmail.com>
2020-06-11 20:38:56 -0400

    Added blog page and ability to add blog

--- lobby.js
+++ lobby.js
@@ -22,6 +22,12 @@ const streamerSchema = new mongoose.Schema({
 });
 const Streamer = mongoose.model("Streamer", streamerSchema);
 
+const blogSchema = new mongoose.Schema({
+    title: String,
+    body: String
+});
+const Blog = mongoose.model("Blog", blogSchema);
+
 const port = process.env.PORT || 3000;
 app.listen(port, function(){
     console.log("Enter through the lobby.");
@@ -65,6 +71,18 @@ Streamer.create({
 });
 */
 
+/*
+Blog.create({
+    title: "First blog post",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+}, function(err, str){
+    if(err){
+        console.log(err);
+    } else {
+        console.log(str);
+    }
+});
+*/
 
 class streamerIcon {
     constructor(iconElement, isLive) {
@@ -75,13 +93,10 @@ class streamerIcon {
 
 function checkIfLive(){
     Streamer.find({}, function(err, str) {
-        console.log("did we make it to this last part?");
         if(err) {
             console.log(err);
         } else {
-            console.log("made it to else of checkiflive");
             str.forEach(function(streamer){
-                console.log("made it in the forloop for " + streamer.login)
                 options.url = 'https://api.twitch.tv/helix/streams?user_login=' + streamer.login;
                 request(options, callback);
             });
@@ -121,7 +136,6 @@ function callback(error, response, body) {
             });
         }
     }
-    console.log("...hello?");
 }
 
 app.get("/", function(req, res){
@@ -134,7 +148,14 @@ app.get("/about", function(req, res) {
 });
 
 app.get("/blog", function(req, res) {
-    res.render("blog");
+    Blog.find({}, function(err, blogs) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log(blogs);
+            res.render("blog", {blogs: blogs});
+        }
+    });
 });
 
 app.get("/streamers", function(req, res) {
@@ -159,7 +180,17 @@ app.get("/streamers/new", function(req, res){
 
 // CREATE
 app.post("/blog", function(req, res){
-
+    Blog.create({
+        title: req.body.title,
+        body: req.body.body
+    }, function(err, blogs){
+        if(err){
+            console.log(err);
+        } else {
+            console.log(blogs);
+            res.redirect('/blog');
+        }
+    })
 });
 
 app.post("/streamers", function(req, res) {
@@ -168,25 +199,55 @@ app.post("/streamers", function(req, res) {
 
 // SHOW
 app.get("/blog/:id", function(req, res) {
-    res.send("Blog post " + req.params.id);
+    Blog.findById(req.params.id, function(err, blog){
+        if(err) {
+            console.log(err);
+        } else {
+            res.render('singleBlog', {blog: blog} );
+        }
+    });
 });
 
 app.get("/streamers/:id", function(req, res){
-    res.send("Streamer #" + req.params.id);
+    Streamer.findById(req.params.id, function(err, str){
+        if(err) {
+            console.log(err);
+        } else {
+            console.log(str);
+        }
+    });
 });
 
 // EDIT
 app.get("/blog/:id/edit", function(req, res) {
-
+    Blog.findById(req.params.id, function(err, blog){
+        if(err) {
+            console.log(err);
+        } else {
+            res.render(editBlog('editBlog', {blog: blog}));
+        }
+    });
 });
 
 app.get("/streamers/:id/edit", function(req, res) {
-
+    Streamers.findById(req.params.id, function(err, str){
+        if(err) {
+            console.log(err);
+        } else {
+            console.log(str);
+        }
+    });
 });
 
 // UPDATE
 app.put("/blog/:id", function(req, res) {
-
+    Blog.findByIdAndUpdate(req.params.id, req.body, function(err, blog){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("/blog");
+        }
+    })
 });
 
 app.put("/streamers/:id",  function(req, res){
@@ -197,7 +258,6 @@ app.put("/streamers/:id",  function(req, res){
             console.log(err);
         } else {
             console.log(doc);
-            console.log("req body1: " + req.body.updatedStreamer);
             res.redirect("/streamers");
         }
     });
@@ -205,9 +265,21 @@ app.put("/streamers/:id",  function(req, res){
 
 // DESTROY
 app.delete("/blog/:id", function(req, res){
-    
+    Blog.findByIdAndRemove(req.params.id, function(err, blog){
+        if(err) {
+            console.log(err);
+        } else {
+            res.redirect("/blog")
+        }
+    });
 });
 
 app.delete("/streamers/:id", function(req, res){
-
+    Streamer.findByIdAndRemove(req.params.id, function(err, str){
+        if(err) {
+            console.log(err);
+        } else {
+            res.redirect("/streamers");
+        }
+    });
 });
